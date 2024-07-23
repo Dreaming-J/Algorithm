e051143051991b4937bac023cc5f12d1c269dfab
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-07-23 15:18:29 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+#백준 2439. 별 찍기 - 2
+N번째 줄에 별 N개를 오른쪽 정렬하여 찍는 문제
+
+#입력
+첫째 줄: N (1 <= N <= 100)
+#출력
+첫째 줄부터 N번째 줄까지 차례대로 별 출력
+
+#로직
+이중 for문으로 별 출력
+
+ */
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder answer = new StringBuilder();
+    static int lineCount;
+    
+    public static void main(String[] args) throws IOException {
+    	lineCount = Integer.parseInt(br.readLine());
+    	
+    	for (int row = 1; row <= lineCount; row++) {
+    		for (int col = 0; col < lineCount; col++) {
+    			if (col < lineCount - row) {
+    				answer.append(" ");
+    				continue;
+    			}
+    			answer.append("*");
+    		}
+    		answer.append("\n");
+    	}
+    	
+    	System.out.println(answer.toString());
+    }
+}
