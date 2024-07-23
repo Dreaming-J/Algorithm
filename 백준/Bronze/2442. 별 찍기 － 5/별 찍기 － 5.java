b99b3d4b0c37fa31527244957fb94d6853d09f56
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-07-23 15:27:44 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 136 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+#백준 2442. 별 찍기 - 5
+첫째 줄에 1개, 둘째 줄에 3개, ..., N번째 줄에 2xN-1개의 별을 가운데 대칭으로 찍는 문제
+
+#입력
+첫째 줄: N (1 <= N <= 100)
+#출력
+첫째 줄부터 N번째 줄까지 차례대로 별 출력
+
+#로직
+이중 for문으로 별 출력
+ */
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder answer = new StringBuilder();
+    static int lineCount;
+    
+    public static void main(String[] args) throws IOException {
+    	lineCount = Integer.parseInt(br.readLine());
+    	
+    	for (int row = 0; row < lineCount; row++) {
+    		for (int col = 1; col < lineCount; col++) {
+    			if (col < lineCount - row) {
+    				answer.append(" ");
+    				continue;
+    			}
+    			answer.append("**");
+    		}
+    		answer.append("*\n");
+    	}
+    	
+    	System.out.println(answer.toString());
+    }
+}
