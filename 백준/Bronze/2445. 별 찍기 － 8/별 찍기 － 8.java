5a0b74afaa5558cd0bc5f8456cc00845afbc379e
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-07-28 23:29:10 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 188 ms, Memory: 16372 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+#백준 2445. 별 찍기 - 8
+예제를 보고 규칙을 유추한 뒤 별 찍기
+    #예제 입력
+    5
+    #예제 출력
+    *        *
+    **      **
+    ***    ***
+    ****  ****
+    **********
+    ****  ****
+    ***    ***
+    **      **
+    *        *
+
+#입력
+첫째 줄: N (1 <= N <= 100)
+#출력
+첫째 줄부터 2xN-1번째 줄까지 차례대로 별 출력
+
+#로직
+이중 for문 2개를 이용해 별 출력
+ */
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int lineCount;
+
+    public static void main(String[] args) throws IOException {
+        lineCount = Integer.parseInt(br.readLine());
+
+        for (int row = 1; row <= lineCount; row++) {
+            System.out.println("*".repeat(row) + " ".repeat(2 * (lineCount - row)) + "*".repeat(row));
+        }
+
+        for (int row = 1; row < lineCount; row++) {
+            System.out.println("*".repeat(lineCount - row) + " ".repeat(2 * row) + "*".repeat(lineCount - row));
+        }
+    }
+}
