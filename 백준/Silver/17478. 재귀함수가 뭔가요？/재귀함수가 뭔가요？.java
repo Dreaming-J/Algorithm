adb3e3a5a47a433f1d1a9d42ed24f8566ac7c9d3
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-12 23:43:10 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 140 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,48 @@
+/*
+#백준 17478. 재귀함수가 뭔가요?
+주어진 예제 출력에 맞춰 재귀함수를 작성하라.
+
+#입력
+첫째 줄: N (1 <= N <= 50)
+#출력
+재귀 횟수에 따른 답변을 출력
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static final String dash = "____";
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int count;
+
+    public static void recursiveChatBot(int n) throws IOException {
+        if (n == count) {
+            output.write(dash.repeat(n) + "\"재귀함수가 뭔가요?\"\n");
+            output.write(dash.repeat(n) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+            output.write(dash.repeat(n) + "라고 답변하였지.\n");
+            return;
+        }
+
+        output.write(dash.repeat(n) + "\"재귀함수가 뭔가요?\"\n");
+        output.write(dash.repeat(n) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+        output.write(dash.repeat(n) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+        output.write(dash.repeat(n) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+        recursiveChatBot(n + 1);
+        output.write(dash.repeat(n) + "라고 답변하였지.\n");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        count = Integer.parseInt(input.readLine());
+
+        output.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        recursiveChatBot(0);
+
+        output.flush();
+        output.close();
+    }
+}
