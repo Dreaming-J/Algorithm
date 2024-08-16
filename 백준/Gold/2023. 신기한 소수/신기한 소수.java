8d9a52dbef7aeff446d653d0f2796ace708d19c7
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-16 12:43:03 +0900

    [Gold V] Title: 신기한 소수, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder output = new StringBuilder();
     static int digits;
     static final int[] NthDigit = {2, 3, 5, 7};
@@ -26,26 +25,28 @@ public class Main {
         return true;
     }
 
-    public static void findAmazingPrime(int depth, int number, int[] canDigits) {
+    public static void findAmazingPrime(int depth, int number) {
         if (depth == digits) {
             output.append(number).append("\n");
         }
 
-        for (int firstDigit : canDigits) {
+        for (int firstDigit = 1; firstDigit <= 9; firstDigit += 2) {
             int newNumber = number * 10 + firstDigit;
             //3. 해당 숫자가 소수인지 판단하여, 소수만 중복 순열을 순회하도록 한다.
             if (isPrime(newNumber))
-                findAmazingPrime(depth + 1, newNumber, canDigits);
+                findAmazingPrime(depth + 1, newNumber);
         }
     }
 
     public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
         //1. 신기한 소수의 자리 수를 입력받는다.
         digits = Integer.parseInt(input.readLine());
 
         //2. {2, 3, 5, 7}로 시작하는 신기한 소수를 중복 순열을 이용해 찾는다.
         for (int num : NthDigit) {
-            findAmazingPrime(1, num, canDigits);
+            findAmazingPrime(1, num);
         }
 
         System.out.println(output.toString());
