46c588fd084e7c7bff37ae175c85aee1bf6cd0a4
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-20 13:28:32 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 733 ms, Memory: 108,636 KB -BaekjoonHub

--- SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
+++ SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -21,63 +21,55 @@ class Solution {
     static final int MIN_INTERVAL_SIZE = 3;
     static int testCases;
     static int mountainSize;
-    static int[] mountains;
     static int start, end;
     static int intervalCount;
-     
-    public static void initTestCase() {
-    	//1. 산의 개수 입력
-    	mountainSize = input.nextInt();
-    	
-    	//2. 산의 개수만큼 높이 입력
-    	mountains = new int[mountainSize];
-    	for (int idx = 0; idx < mountainSize; idx++)
-    		mountains[idx] = input.nextInt();
-    	
-    	//3. 변수 초기화
-    	start = 1;
-    	end = 0;
-    	intervalCount = 0;
-    }
-
-	//4. 구간 찾기
-    public static void findInterval() {
-    	for (int idx = 1; idx < mountainSize; idx++) {
-	    	//4-1. 이전 값보다 크다면
-    		if (mountains[idx] > mountains[idx - 1]) {
-		    	//4-1-1. 끝 구간의 개수가 0이라면, 시작 구간 개수 증가
-    			if (end == 0)
-    				start++;
-    			
-		    	//4-1-2. 끝 구간의 개수가 0이 아니라면,
-    			else {
-    				//4-1-2-1. 구간의 길이가 3이상이면 구간의 개수 계산
-    				if (start + end >= MIN_INTERVAL_SIZE)
-    					intervalCount += (start - 1) * end;
-					start = 2;
-					end = 0;
-    			}
-    		}
-    		
-	    	//4-2. 이전 값보다 작으면 끝 구간 개수 증가
-    		else if (mountains[idx] < mountains[idx - 1])
-    			end++;
-    	}
-    	
-    	//4-3. 탐색 종료 후, 아직 구간이 남아있다면 구간의 개수 계산
-    	if (start + end >= MIN_INTERVAL_SIZE) {
-			intervalCount += (start - 1) * end;
-    	}
-    }
+    static int prevHeight, curHeight;
  
     public static void main(String args[]) {
         //0. 테스트 케이스 입력
         testCases = input.nextInt();
         for(int tc = 1; tc <= testCases; tc++) {
-            initTestCase();
-
-        	//4. 구간 찾기
-            findInterval();
+        	//1. 변수 초기화
+        	start = 1;
+        	end = 0;
+        	intervalCount = 0;
+        	
+        	//2. 산의 개수 입력
+        	mountainSize = input.nextInt();
+        	
+        	//3. 산의 개수만큼 높이 입력
+        	prevHeight = input.nextInt();
+        	for (int idx = 1; idx < mountainSize; idx++) {
+        		//4. 구간 찾기
+        		curHeight = input.nextInt();
+        		
+    	    	//4-1. 이전 값보다 크다면
+        		if (curHeight > prevHeight) {
+    		    	//4-1-1. 끝 구간의 개수가 0이라면, 시작 구간 개수 증가
+        			if (end == 0)
+        				start++;
+        			
+    		    	//4-1-2. 끝 구간의 개수가 0이 아니라면,
+        			else {
+        				//4-1-2-1. 구간의 길이가 3이상이면 구간의 개수 계산
+        				if (start + end >= MIN_INTERVAL_SIZE)
+        					intervalCount += (start - 1) * end;
+    					start = 2;
+    					end = 0;
+        			}
+        		}
+        		
+    	    	//4-2. 이전 값보다 작으면 끝 구간 개수 증가
+        		else if (curHeight < prevHeight)
+        			end++;
+        		
+        		prevHeight = curHeight;
+        	}
+        	
+        	//4-3. 탐색 종료 후, 아직 구간이 남아있다면 구간의 개수 계산
+        	if (start + end >= MIN_INTERVAL_SIZE) {
+    			intervalCount += (start - 1) * end;
+        	}
              
             output.append("#").append(tc).append(" ").append(intervalCount).append("\n");
         }
