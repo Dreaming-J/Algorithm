25e33dbd47e8aa76284d984eac4e1b94a4e021da
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-22 18:11:19 +0900

    [D4] Title: 정사각형 방, Time: 344 ms, Memory: 36,432 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -7,13 +7,9 @@ SWEA 1861. 정사각형 방
 	1-2. 방의 정보 입력
 	1-3. 변수 초기화
 2. dfs을 이용해 모든 방의 경우의 수 탐색
-	2-1. 방문한 방의 갯수가 최대와 동일하다면 업데이트
-	2-2. 방문한 방의 갯수가 최대라면 업데이트
-	2-3. 4방 탐색으로 다음 방 이동
-    	2-3-1. 범위를 벗어났다면 패스
-		2-3-2. 이미 방문한 방이라면 패스
-    	2-3-3. 이동할 방이 현재 방 번호보다 1큰게 아니라면 패스
-    	2-3-4. 4방으로 이동
+	2-1. 범위를 벗어났다면 패스
+	2-2. 이동할 방이 현재 방 번호보다 1큰게 아니라면 패스
+	2-3. 4방으로 이동
 3. 출력
  */
  
@@ -30,8 +26,7 @@ public class Solution {
     static int testCase;
     static int roomSize;
     static int[][] room;
-    static boolean[][] visit;
-    static int maxRoomCount, minRoomIdx;
+    static int maxRoomCount, minRoomIdx, roomCount;
  
     public static void initTestCase() throws IOException {
     	//1-1. 방의 크기 입력
@@ -46,43 +41,26 @@ public class Solution {
     	}
     	
     	//1-3. 변수 초기화
-    	visit = new boolean[roomSize][roomSize];
     	maxRoomCount = Integer.MIN_VALUE;
     	minRoomIdx = Integer.MAX_VALUE;
     }
     
-    public static void findBestRoute(int row, int col, int roomCount, int startRoomIdx) {
-    	//2-1. 방문한 방의 갯수가 최대와 동일하다면 업데이트
-    	if (roomCount == maxRoomCount)
-    		minRoomIdx = Math.min(startRoomIdx, minRoomIdx);
-    	
-    	//2-2. 방문한 방의 갯수가 최대라면 업데이트
-    	if (roomCount > maxRoomCount) {
-    		maxRoomCount = roomCount;
-    		minRoomIdx = startRoomIdx;
-    	}
-    	
-    	//2-3. 4방 탐색으로 다음 방 이동
+    public static void findBestRoute(int row, int col) {
     	for (int[] delta : deltas) {
     		int nr = row + delta[0];
     		int nc = col + delta[1];
     		
-    		//2-3-1. 범위를 벗어났다면 패스
+    		//2-1. 범위를 벗어났다면 패스
     		if (nr < 0 || nr >= roomSize || nc <0 || nc >= roomSize)
     			continue;
-    		
-			//2-3-2. 이미 방문한 방이라면 패스
-			if (visit[nr][nc])
-				continue;
 			
-			//2-3-3. 이동할 방이 현재 방 번호보다 1큰게 아니라면 패스
+			//2-2. 이동할 방이 현재 방 번호보다 1큰게 아니라면 패스
 			if (room[nr][nc] != room[row][col] + 1)
 				continue;
 			
-			//2-3-4. 4방으로 이동
-			visit[nr][nc] = true;
-			findBestRoute(nr, nc, roomCount + 1, startRoomIdx);
-			visit[nr][nc] = false;
+			//2-3. 4방으로 이동
+			roomCount++;
+			findBestRoute(nr, nc);
     	}
     }
  
@@ -97,9 +75,19 @@ public class Solution {
             //2. dfs을 이용해 모든 방의 경우의 수 탐색
             for (int row = 0; row < roomSize; row++) {
 	    		for (int col = 0; col < roomSize; col++) {
-	    			visit[row][col] = true;
-	    			findBestRoute(row, col, 1, room[row][col]);
-	    			visit[row][col] = false;
+	    			roomCount = 1;
+                    
+	    			findBestRoute(row, col);
+	    			
+	    			//2-4. 방문 횟수가 동일하다면, 시작 위치만 업데이트
+	    	    	if (roomCount == maxRoomCount)
+	    	    		minRoomIdx = Math.min(room[row][col], minRoomIdx);
+	    	    	
+	    	    	//2-5. 방문 횟수가 더 크다면, 최대 방문 횟수와 시작 위치 업데이트
+	    	    	if (roomCount > maxRoomCount) {
+	    	    		maxRoomCount = roomCount;
+	    	    		minRoomIdx = room[row][col];
+	    	    	}
 	    		}
 	    	}
             
