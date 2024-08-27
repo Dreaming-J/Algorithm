26561ca06e630c6c59a513cd0457726fb52d1ba9
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-27 13:01:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 379 ms, Memory: 50,980 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -2,10 +2,10 @@
 =SWEA 2382. [모의 SW 역량테스트] 미생물 격리
 
 1. 테스트케이스 입력
-2. 구역의 길이, 격리 시간, 미생물 군집의 개수 입력
-3. 미생물 군집의 개수만큼 세로 위치, 가로 위치, 미생물 수, 이동 방향 입력
-4. 변수 초기화
-5. 시뮬레이션 통해 시간에 따른 미생물 군집 이동 계산
+	1-1. 구역의 길이, 격리 시간, 미생물 군집의 개수 입력
+	1-2. 미생물 군집의 개수만큼 세로 위치, 가로 위치, 미생물 수, 이동 방향 입력
+	1-3. 변수 초기화
+2. 시뮬레이션 통해 시간에 따른 미생물 군집 이동 계산
  */
 
 import java.io.BufferedReader;
@@ -18,7 +18,6 @@ public class Solution {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder output = new StringBuilder();
     static StringTokenizer st;
-    static final int UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
     static final int[][] deltas = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}}; //상하좌우 배열
     static int testCase;
     static int length, time, groupSize;
@@ -30,9 +29,9 @@ public class Solution {
         int row;
         int col;
         int germCount;
-        int direction;
+        Direction direction;
 
-        public Germ(int row, int col, int germCount, int direction) {
+        public Germ(int row, int col, int germCount, Direction direction) {
             this.row = row;
             this.col = col;
             this.germCount = germCount;
@@ -40,8 +39,8 @@ public class Solution {
         }
 
         public void move() {
-            this.row += deltas[direction][0];
-            this.col += deltas[direction][1];
+            this.row += deltas[direction.idx][0];
+            this.col += deltas[direction.idx][1];
 
             if (isBorder(row, col)) {
                 germCount /= 2;
@@ -50,14 +49,7 @@ public class Solution {
         }
 
         public void changeDirection() {
-            if (direction == UP)
-                this.direction = DOWN;
-            else if (direction == DOWN)
-                this.direction = UP;
-            else if (direction == LEFT)
-                this.direction = RIGHT;
-            else if (direction == RIGHT)
-                this.direction = LEFT;
+        	direction = Direction.values()[direction.reverseIdx];
         }
 
         @Override
@@ -65,27 +57,29 @@ public class Solution {
             return -(this.germCount - o.germCount);
         }
     }
-
-    public static void initTestCase() throws IOException {
-        //2. 구역의 길이, 격리 시간, 미생물 군집의 개수 입력
-        st = new StringTokenizer(input.readLine().trim());
-        length = Integer.parseInt(st.nextToken());
-        time = Integer.parseInt(st.nextToken());
-        groupSize = Integer.parseInt(st.nextToken());
-
-        //3. 미생물 군집의 개수만큼 세로 위치, 가로 위치, 미생물 수, 이동 방향 입력
-        map = new Germ[length][length];
-        germs = new PriorityQueue<>();
-        for (int idx = 0; idx < groupSize; idx++) {
-            st = new StringTokenizer(input.readLine().trim());
-            Germ germ = new Germ(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            map[germ.row][germ.col] = germ;
-            germs.add(germ);
-        }
-
-        //4. 변수 초기화
-        totalGermCount = 0;
+    
+    public static enum Direction {
+    	UP(1, 1),
+    	DOWN(2, 0),
+    	LEFT(3, 3),
+    	RIGHT(4, 2);
+    	
+    	final int idx;
+    	final int reverseIdx;
+		
+    	private Direction(int idx, int reverse) {
+			this.idx = idx;
+			this.reverseIdx = reverse;
+		}
+    	
+    	public static Direction of(int idx) {
+    		for (Direction direction : values()) {
+    			if (direction.idx == idx)
+    				return direction;
+    		}
+    		
+    		return null;
+    	}
     }
 
     public static void simulation() {
@@ -125,7 +119,7 @@ public class Solution {
         for (int tc = 1; tc <= testCase; tc++) {
             initTestCase();
 
-            //5. 시뮬레이션 통해 시간에 따른 미생물 군집 이동 계산
+            //2. 시뮬레이션 통해 시간에 따른 미생물 군집 이동 계산
             for (int cur = 0; cur < time; cur++) {
                 simulation();
             }
@@ -137,5 +131,27 @@ public class Solution {
             output.append("#").append(tc).append(" ").append(totalGermCount).append("\n");
         }
         System.out.println(output);
+    }
+
+    public static void initTestCase() throws IOException {
+        //1-1. 구역의 길이, 격리 시간, 미생물 군집의 개수 입력
+        st = new StringTokenizer(input.readLine().trim());
+        length = Integer.parseInt(st.nextToken());
+        time = Integer.parseInt(st.nextToken());
+        groupSize = Integer.parseInt(st.nextToken());
+
+        //1-2. 미생물 군집의 개수만큼 세로 위치, 가로 위치, 미생물 수, 이동 방향 입력
+        map = new Germ[length][length];
+        germs = new PriorityQueue<>();
+        for (int idx = 0; idx < groupSize; idx++) {
+            st = new StringTokenizer(input.readLine().trim());
+            Germ germ = new Germ(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()), Direction.of(Integer.parseInt(st.nextToken())));
+            map[germ.row][germ.col] = germ;
+            germs.add(germ);
+        }
+
+        //1-3. 변수 초기화
+        totalGermCount = 0;
     }
 }
