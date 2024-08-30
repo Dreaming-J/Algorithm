6dfe9f6b43718bb1bec1e8cfd0e2657a9ae225ae
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-08-30 15:24:02 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,168 ms, Memory: 112,648 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,7 +3,7 @@
 
 =특이사항
 정점은 1번부터 시작
-크루스칼
+프림
 
 =로직
 0. 테스트 케이스 개수 입력
@@ -11,7 +11,7 @@
 	1-1. 정점의 개수와 간선의 개수 입력
 	1-2. 간선의 개수만큼 간선 정보 입력
 	1-3. 변수 초기화
-2. 크루스칼 알고리즘을 적용한 MST 계산
+2. 프림 알고리즘을 적용한 MST 계산
 3. 출력
  */
 
@@ -27,35 +27,49 @@ public class Solution {
     static StringTokenizer st;
     static int testCase;
     static int vertexSize, edgeSize;
-    static PriorityQueue<Edge> edges;	//각 섬 사이의 간선을 관리하는 배열
-    static int[] parents;				//서로소 집합용 배열
-    static int[] rank;					//서로소 집합의 랭크를 관리하는 배열
+    static Node[] vertices;
     
-    public static class Edge {
-    	int start, end, weight;
+    public static class Node implements Comparable<Node> {
+    	int to, weight;
+    	Node next;
 
-		public Edge(int start, int end, int weight) {
-			this.start = start;
-			this.end = end;
+		public Node(int to, int weight, Node next) {
+			this.to = to;
 			this.weight = weight;
+			this.next = next;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
 		}
     }
     
-    //2. 크루스칼 알고리즘을 적용한 MST 계산
+    //2. 프림 알고리즘을 적용한 MST 계산
     public static long calMST() {
-    	int count = 0;
-    	long cost = 0;
-    	
-    	while (!edges.isEmpty()) {
-    		Edge cur = edges.poll();
-    		
-    		if (union(cur.start, cur.end)) {
-    			cost += cur.weight;
-    			
-    			if (++count == vertexSize - 1)
-    				break;
-    		}
-    	}
+		long cost = 0;
+		
+		boolean[] visited = new boolean[vertexSize + 1];
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		queue.add(new Node(1, 0, null));
+
+		int cnt = 0;
+		while (true) {
+			Node cur = queue.poll();
+			
+			if (visited[cur.to])
+				continue;
+			visited[cur.to]= true;
+			
+			cost += cur.weight;
+			
+			if (++cnt == vertexSize)
+				break;
+			
+			for (Node next = vertices[cur.to]; next != null; next = next.next) {
+				queue.add(next);
+			}
+		}
     	
     	return cost;
     }
@@ -72,32 +86,6 @@ public class Solution {
         }
         System.out.println(output);
     }
-    
-    public static boolean union(int element1, int element2) {
-    	int parent1 = find(element1);
-    	int parent2 = find(element2);
-    	
-    	if (parent1 == parent2)
-    		return false;
-    	
-    	if (rank[parent1] > rank[parent2]) {
-    		parents[parent2] = parent1;
-    		return true;
-    	}
-    	
-    	if (rank[parent1] == rank[parent2])
-    		rank[parent2]++;
-    	
-    	parents[parent1] = parent2;
-    	return true;
-    }
-    
-    public static int find(int element) {
-    	if (parents[element] == 0)
-    		return element;
-    	
-    	return parents[element] = find(parents[element]);
-    }
 
     public static void initTestCase() throws IOException {
     	//1-1. 정점의 개수와 간선의 개수 입력
@@ -106,14 +94,15 @@ public class Solution {
     	edgeSize = Integer.parseInt(st.nextToken());
     	
     	//1-2. 간선의 개수만큼 간선 정보 입력
-    	edges = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
+    	vertices = new Node[vertexSize + 1];
     	for (int idx = 0; idx < edgeSize; idx++) {
     		st = new StringTokenizer(input.readLine().trim());
-    		edges.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+    		int from = Integer.parseInt(st.nextToken());
+    		int to = Integer.parseInt(st.nextToken());
+    		int weight = Integer.parseInt(st.nextToken());
+    		
+    		vertices[from] = new Node(to, weight, vertices[from]);
+    		vertices[to] = new Node(from, weight, vertices[to]);
     	}
-
-    	//1-3. 변수 초기화
-    	parents = new int[vertexSize + 1];
-    	rank = new int[vertexSize + 1];
     }
 }
