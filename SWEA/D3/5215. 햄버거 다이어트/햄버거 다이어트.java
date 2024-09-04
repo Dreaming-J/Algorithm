5f3e17019ade2abb59cc0c96299c48f306928cd0
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-09-04 17:27:07 +0900

    [D3] Title: 햄버거 다이어트, Time: 131 ms, Memory: 19,852 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,76 +1,80 @@
 /*
-=SWEA 5215. 햄버거 다이어트
+= SWEA 5215. 햄버거 다이어트
 
-0. 테스트 케이스를 입력받는다.
-1. 입력
-	1-1. 재료의 수, 제한 칼로리를 입력받는다.
-	1-2. N개의 줄에 걸쳐 재료의 점수와 칼로리를 입력받는다.
-2. 초기화
-3. 재료의 부분집합을 구한다.
-	3-1. 제한 칼로리를 넘어가는 조합은 구하지 않는다.
-	3-2. 모든 재료를 탐색했다면 종료한다.
-		3-2-1. 최고 점수라면 갱신한다.
+= 로직
+0. 테스트 케이스 입력
+1. 초기 세팅
+	1-1. 재료의 수, 제한 칼로리 입력
+	1-2. N개의 줄에 걸쳐 재료의 점수와 칼로리 입력
+	1-3. 초기화
+2. 최고 점수 탐색
+3. 출력
 */
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.IOException;
 
 public class Solution {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder output = new StringBuilder();
-	static StringTokenizer st;
-	static int ingredientCount, limitKcal, maxScore;
-	static int[][] ingredient; //점수, 칼로리 보관
-	
-	public static void initTestCase() throws IOException {
-		//1. 입력
-		//1-1. 재료의 수, 제한 칼로리를 입력받는다.
-		st = new StringTokenizer(input.readLine().trim());
-		ingredientCount = Integer.parseInt(st.nextToken());
-		limitKcal = Integer.parseInt(st.nextToken());
-		
-		//1-2. N개의 줄에 걸쳐 재료의 점수와 칼로리를 입력받는다.
-		ingredient = new int[ingredientCount][2];
-		for (int idx = 0; idx < ingredientCount; idx++) {
-			st = new StringTokenizer(input.readLine().trim());
-			ingredient[idx][0] = Integer.parseInt(st.nextToken());
-			ingredient[idx][1] = Integer.parseInt(st.nextToken());
-		}
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+    static StringTokenizer st;
+    static int ingredientSize, limitKcal;
+    static Ingredient[] ingredients;
+    static int[] bestScoreAboutKcal;
 
-		//2. 초기화
-		maxScore = Integer.MIN_VALUE;
-	}
-	
-	public static void powerSet(int elementIdx, int kcal, int score) {
-		//3-1. 제한 칼로리를 넘어가는 조합은 구하지 않는다.
-		if (kcal > limitKcal)
-			return;
-		
-		//3-2. 모든 재료를 탐색했다면 종료한다.
-		if (elementIdx == ingredientCount) {
-			//3-2-1. 최고 점수라면 갱신한다.
-			maxScore = Math.max(score , maxScore);
-			return;
-		}
-		
-		powerSet(elementIdx + 1, kcal + ingredient[elementIdx][1], score + ingredient[elementIdx][0]);
-		powerSet(elementIdx + 1, kcal, score);
-	}
-	
     public static void main(String[] args) throws IOException {
-    	//0. 테스트 케이스를 입력받는다.
-    	int testCase = Integer.parseInt(input.readLine());
-    	
-    	for (int tc = 1; tc <= testCase; tc++) {
-    		initTestCase();
+        //0. 테스트 케이스 입력
+        int testCase = Integer.parseInt(input.readLine());
 
-			//3. 재료의 부분집합을 구한다.
-            powerSet(0, 0, 0);
-    		
-    		output.append("#").append(tc).append(" ").append(maxScore).append("\n");
-    	}
-    	System.out.println(output);
+        for (int tc = 1; tc <= testCase; tc++) {
+            //1. 초기 세팅
+            initTestCase();
+            
+            //2. 최고 점수 탐색
+            for (Ingredient ingredient : ingredients) {
+                //앞의 값을 참고하기 때문에 오염 방지를 위해 뒤에서부터 탐색
+	        	for (int kcal = limitKcal; kcal > 0; kcal--) {
+	        		if (kcal < ingredient.kcal)
+	        			bestScoreAboutKcal[kcal] = bestScoreAboutKcal[kcal - 1];
+	        		else
+	        			bestScoreAboutKcal[kcal] = Math.max(bestScoreAboutKcal[kcal - ingredient.kcal] + ingredient.score, bestScoreAboutKcal[kcal]);
+	        	}
+            }
+
+            //3. 출력
+            output.append("#").append(tc).append(" ").append(bestScoreAboutKcal[limitKcal]).append("\n");
+        }
+        System.out.println(output);
     }
+    
+    public static class Ingredient {
+    	int score, kcal;
+
+		public Ingredient(int score, int kcal) {
+			this.score = score;
+			this.kcal = kcal;
+		}
+    }
+
+    //1. 초기 세팅
+    public static void initTestCase() throws IOException {
+        //1-1. 재료의 수, 제한 칼로리 입력
+        st = new StringTokenizer(input.readLine().trim());
+        ingredientSize = Integer.parseInt(st.nextToken());
+        limitKcal = Integer.parseInt(st.nextToken());
+        
+        //1-2. N개의 줄에 걸쳐 재료의 점수와 칼로리 입력
+        ingredients = new Ingredient[ingredientSize];
+        for (int idx = 0; idx < ingredientSize; idx++) {
+            st = new StringTokenizer(input.readLine().trim());
+            ingredients[idx] = new Ingredient(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        //1-3. 초기화
+        bestScoreAboutKcal = new int[limitKcal + 1];
+    }
+    
+    
 }
