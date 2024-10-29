3f9e60109f9a422228dcc6564a35d84d0d9c86a1
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2024-10-29 17:48:53 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 16144 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,42 @@
+/*
+= BOJ 9252. LCS 2
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+    static String string1, string2;
+    static int[][] lcs;
+    static int string1Size, string2Size;
+    
+    public static void main(String[] args) throws IOException {
+    	init();
+    	
+    	for (int row = 1; row <= string1Size; row++) {
+    		for (int col = 1; col <= string2Size; col++) {
+    			if (string1.charAt(row - 1) == string2.charAt(col - 1))
+    				lcs[row][col] = lcs[row - 1][col - 1] + 1;
+    			else
+    				lcs[row][col] = Math.max(lcs[row][col - 1], lcs[row - 1][col]);
+    		}
+    	}
+        
+    	output.append(lcs[string1Size][string2Size]);
+    	
+    	System.out.println(output);
+    }
+
+    public static void init() throws IOException {
+    	string1 = input.readLine();
+    	string2 = input.readLine();
+    	
+    	string1Size = string1.length();
+    	string2Size = string2.length();
+    	
+    	lcs = new int[string1Size + 1][string2Size + 1];
+    }
+}
