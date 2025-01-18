0062671c3fad8cfc919db2aa3e3e5af8b9e63961
김종헌 <78305338+Dreaming-J@users.noreply.github.com>
2025-01-18 23:20:27 +0900

    [Gold II] Title: 동전 분배, Time: 2008 ms, Memory: 12668 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,15 +1,6 @@
 /*
 = BOJ 1943. 동전 분배
-
-= 로직
-1. 초기 세팅
-    1-1. 동전의 종류 입력
-    1-2. 동전의 금액과 개수 입력
-    1-3. 목표 비용 계산
-    1-4. 변수 초기화
-2. 분배 가능 여부 탐색
-3. 출력
- */
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,47 +8,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int TEST_CASE_SIZE = 3;
+
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder output = new StringBuilder();
     static StringTokenizer st;
-    static final int TEST_CASE = 3;
-    static int coinSize, targetCost;
-    static boolean isInteger;
+    static int coinSize;
     static Coin[] coins;
+    static int targetCost;
     static int[] dp;
 
-    public static void findTargetCost() {
-        if (!isInteger) {
-            return;
-        }
-
-        for (Coin coin : coins) {
-            for (int count = 1; count <= coin.count; count++) {
-                for (int cost = targetCost; cost >= coin.cost * count; cost--) {
-                    dp[cost] = Math.max(dp[cost - coin.cost], dp[cost]);
-
-                    if (dp[targetCost] == 1)
-                        return;
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        for (int tc = 0; tc < TEST_CASE; tc++) {
-            //1. 초기 세팅
-            init();
-
-            //2. 분배 가능 여부 탐색
-            findTargetCost();
-
-            //3.출력
-            output.append(dp[targetCost]).append("\n");
-        }
-        System.out.print(output);
-    }
-
-    public static class Coin {
+    static class Coin {
         int cost, count;
 
         public Coin(int cost, int count) {
@@ -66,26 +27,50 @@ public class Main {
         }
     }
 
-    //1. 초기 세팅
-    public static void init() throws IOException {
-        //1-1. 동전의 종류 입력
-        coinSize = Integer.parseInt(input.readLine());
-
-        //1-2. 동전의 금액과 개수 입력
-        coins = new Coin[coinSize];
-        double totalCost = 0;
-        for (int idx = 0; idx < coinSize; idx++) {
-            st = new StringTokenizer(input.readLine());
-            coins[idx] = new Coin(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            totalCost += coins[idx].cost * coins[idx].count;
+    public static void main(String[] args) throws IOException {
+        for (int tc = 0; tc < TEST_CASE_SIZE; tc++) {
+            if (!init()) {
+                output.append(0)
+                        .append("\n");
+                continue;
+            }
+            
+            for (Coin coin : coins) {
+                for (int count = 1; count <= coin.count; count++) {
+                    for (int cost = targetCost; cost >= coin.cost * count; cost--) {
+                        dp[cost] = Math.max(dp[cost - coin.cost], dp[cost]);
+                    }
+                }
+            }            
+            
+            output.append(dp[targetCost])
+                    .append("\n");
         }
 
-        //1-3. 목표 비용 계산
-        targetCost = (int) totalCost / 2;
-        isInteger = totalCost == targetCost * 2;
+        System.out.println(output);
+    }
 
-        //1-4. 변수 초기화
+    private static boolean init() throws IOException {
+        coinSize = Integer.parseInt(input.readLine());
+
+        coins = new Coin[coinSize];
+        targetCost = 0;
+        for (int idx = 0; idx < coinSize; idx++) {
+            st = new StringTokenizer(input.readLine());
+            int cost = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
+
+            coins[idx] = new Coin(cost, count);
+            targetCost += cost * count;
+        }
+        
+        if (targetCost % 2 != 0)
+            return false;
+        
+        targetCost /= 2;
         dp = new int[targetCost + 1];
         dp[0] = 1;
+        
+        return true;
     }
 }
